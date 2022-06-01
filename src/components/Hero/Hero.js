d158437fb5a3c9d3d72ee24310c08ca224a9a2fd
tonyxuan2021 <tonyyexuan@gmail.com>
2022-06-01 09:31:04 -0700

    initialize github

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className='hero__wrapper'>
         <h2>Full Stack Developer</h2>
-        <p>Welcome! If you are a business seeking for a full stack web developer, please contact me.</p>
+        <p className='hero__text'>Welcome! My name is Tony Ye, and I am currently making a career transition from an accountant to a full stack web developer. Beside coding, I enjoy gardening and playing with my pets. If you are a business looking to hire, please contact me.</p>
     </div>
   )
 }
