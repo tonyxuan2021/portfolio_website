18a39386cd6c64bff0072835498dc2f1755f5117
tonyxuan2021 <tonyyexuan@gmail.com>
2022-06-29 08:48:54 -0700

    update portfolio description

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,6 +1,31 @@
 const review = [
   {
     id: 1,
+    title: "Movie App",
+    desc: "A full-stack web application for user to login and browse their favoriate shows",
+    image: "https://assets.codepen.io/7439974/movie.jpg",
+    link: "https://movie-entertain.herokuapp.com/",
+    github: "https://github.com/tonyxuan2021/movie-entertainment-app",
+  },
+
+  {
+    id: 2,
+    title: "Country Wikipedia",
+    desc: "A frontend web application for the users to search for countiries info",
+    image: "https://assets.codepen.io/7439974/earth.jpg",
+    link: "https://country-wikipedia.herokuapp.com/",
+    github: "https://github.com/tonyxuan2021/country-search-app",
+  },
+  {
+    id: 3,
+    title: "Github Devfinder",
+    desc: "A frontend web application to find web developers worldwide",
+    image: "https://assets.codepen.io/7439974/github.jpg",
+    link: "https://github-user-engine.herokuapp.com/",
+    github: "https://github.com/tonyxuan2021/github-user-search",
+  },
+  {
+    id: 4,
     title: "The Booktown",
     desc: "A full-stack web application to buy and sell books",
     image: "https://assets.codepen.io/7439974/bookpic.jpg",
@@ -8,37 +33,13 @@ const review = [
     github: "https://github.com/tonyxuan2021/xuan-ye-the-booktown-frontend",
   },
   {
-    id: 2,
+    id: 5,
     title: "The Hotel Finder",
     desc: "A web application to browse hotels according to city name worldwide",
     image: "https://assets.codepen.io/7439974/hotelpic.jpg",
     link: "https://hackthon-hotel-finder.herokuapp.com/",
     github:
       "https://github.com/tonyxuan2021/BrainStation_Hackthon_new/tree/develop",
-  },
-  {
-    id: 3,
-    title: "Country Wikipedia",
-    desc: "A passion project to help the users search for useful information of a country",
-    image: "https://assets.codepen.io/7439974/earth.jpg",
-    link: "https://country-wikipedia.herokuapp.com/",
-    github: "https://github.com/tonyxuan2021/country-search-app",
-  },
-  {
-    id: 4,
-    title: "Github Devfinder",
-    desc: "A passion project to find world web developers",
-    image: "https://assets.codepen.io/7439974/github.jpg",
-    link: "https://github-user-engine.herokuapp.com/",
-    github: "https://github.com/tonyxuan2021/github-user-search",
-  },
-  {
-    id: 5,
-    title: "Movie App",
-    desc: "A web application that allow user to login and browse their bookmarked movies",
-    image: "https://assets.codepen.io/7439974/movie.jpg",
-    link: "https://movie-entertain.herokuapp.com/",
-    github: "https://github.com/tonyxuan2021/movie-entertainment-app",
   },
 ];
 
